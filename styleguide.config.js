58d0314598114bebd2441b09be5a2bc59dba42d2
Ionut Bostan <ionut.bostan@MBPdeSngular3.home>
2018-07-27 18:26:11 +0200

    Add initial files and configs

---
+++ styleguide.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  template: {
+    head: {
+      links: [
+        {
+          rel: "stylesheet",
+          href:
+            "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        }
+      ]
+    }
+  }
+};
