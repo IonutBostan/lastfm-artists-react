c28f6355e95c51823df5a0644fec648c469ba97d
Ionut Bostan <ionut.bostan@MBPdeSngular3.home>
2018-07-28 17:08:27 +0200

    Remove console logs

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -4,7 +4,6 @@ import "./Track.css";
 export const Track = ({ track, popularity }) => (
   <div className="track">
     <div className="popularity" style={{ width: 343 * popularity }}>
-      {console.log(popularity)}
       <span>
         {track["@attr"].rank} {track.name}
       </span>
