a574739012db3d8fb0a5c0d4c101c5bb6ca0e1c3
Ionut Bostan <ionut.bostan@MBPdeSngular3.home>
2018-07-28 13:15:21 +0200

    update env vars

--- src/actions/actionsUtils.js
+++ src/actions/actionsUtils.js
@@ -1,16 +1,17 @@
-export const getApiKey = () => process.env.LASTFM_KEY;
-export const getApiUrl = () => process.env.LASTFM_URL;
+export const getApiKey = () => process.env.REACT_APP_LASTFM_KEY;
+export const getApiUrl = () => process.env.REACT_APP_LASTFM_URL;
 
-const addLimit = (limit) => limit ? "&limit="+limit : ""
+const addLimit = limit => (limit ? "&limit=" + limit : "");
 
-export const getResourceByMbid = (mbid, method, limit=null) =>
+export const getResourceByMbid = (mbid, method, limit = null) =>
   getApiUrl() +
   "/2.0/?method=" +
   method +
   "&format=json&mbid=?" +
   mbid +
   "&api_key=" +
-  getApiKey() + addLimit(limit)
+  getApiKey() +
+  addLimit(limit);
 
 export const getResourceByCountry = (country, method) =>
   getApiUrl() +
